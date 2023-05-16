update, fix of the chebyshev derivative

Sun/debug_chebyshev_2D.py
@@ -328,7 +328,7 @@ fig.colorbar(surf, shrink=0.5, aspect=5)
 fig.suptitle(r'$\partial z / \partial y$')
 
 
-def ChebyshevDerivativeMatrixBayliss(x):
+def ChebyshevDerivativeMatrix(x):
     """
     Define the first order derivative matrix, where x is the array of Gauss-Lobatto points
     """
@@ -336,29 +336,42 @@ def ChebyshevDerivativeMatrixBayliss(x):
     D = np.zeros((N,N))
     for i in range(0,N):
         for j in range(0,N):
-            xi = np.cos(np.pi*i/N)
-            xj = np.cos(np.pi*j/N)
-            # compute off-diagonal before
-            if (i!=j):
-                if (i==0 or i==N):
-                    ci = 2
-                elif (i>0 or i <N):
-                    ci =1
-                else:
-                    raise ValueError('Some mistake in the computation of the matrix')
-                
-                if (j==0 or j==N):
-                    cj = 2
-                elif (j>0 or j<N):
-                    cj =1
-                else:
-                    raise ValueError('Some mistake in the computation of the matrix')
-                    
-                D[i,j] = (ci/cj)*(-1)**(i+j)/(xi-xj)
-    for i in range(0,N):
-        tot_coeff = np.sum(D[i,:])
-        D[i,i] = - tot_coeff
+            xi = np.cos(np.pi*i/(N-1))
+            xj = np.cos(np.pi*j/(N-1))
             
+            #select the right ci, cj
+            if i==0 or i==N-1:
+                ci = 2
+            else:
+                ci = 1
+            if j==0 or j==N-1:
+                cj = 2
+            else:
+                cj = 1
+            
+            #matrix coefficients
+            if (i!=j):
+                D[i,j] = (ci/cj) * (-1)**(i+j) / (xi-xj)
+            elif (i==j and i>0 and i<N-1):
+                D[i,j] = -xi/2/(1-xi**2)
+            elif (i==0 and j==0):
+                D[i,j] = (2*(N**2)+1)/6
+            elif(i==N-1 and j==N-1):
+                D[i,j] = -(2*(N**2)+1)/6
+            else:
+                raise ValueError('Some mistake in the computation of the matrix')
+            
+    return D
+
+def ChebyshevDerivativeMatrixBayliss(x):
+    """
+    Define the first order derivative matrix, where x is the array of Gauss-Lobatto points
+    """
+    N = len(x) #dimension of the square matrix
+    D = ChebyshevDerivativeMatrix(x)
+    for i in range(0,N):
+        row_tot = np.sum(D[i,:]) - D[i,i]
+        D[i,i] = -row_tot
     return D
 
 

Sun/main_compressor.py
@@ -18,24 +18,41 @@ my_compressor = Compressor('data/eckardt_impeller.csv')
 # my_compressor.AddDataSetZone('data/eckardt_inlet.csv')
 # my_compressor.AddDataSetZone('data/eckardt_outlet.csv')
 
-#3D scatter plots
-my_compressor.scatterPlot3D('theta')
-my_compressor.scatterPlot3D('density')
-my_compressor.scatterPlot3D('radial')
-my_compressor.scatterPlot3D('tangential')
-my_compressor.scatterPlot3D('axial')
-my_compressor.scatterPlot3D('pressure')
+# #3D scatter plots
+# my_compressor.scatterPlot3D('theta')
+# my_compressor.scatterPlot3D('density')
+# my_compressor.scatterPlot3D('radial')
+# my_compressor.scatterPlot3D('tangential')
+# my_compressor.scatterPlot3D('axial')
+# my_compressor.scatterPlot3D('pressure')
 
-#full annulus representation
-my_compressor.scatterPlot3DFull(20, 20, field='pressure',slices=500)
+# #full annulus representation
+# my_compressor.scatterPlot3DFull(20, 20, field='pressure',slices=500)
 
-#circumferential average and 2D plots
-my_compressor.UnstructuredCircumferentialAverage(50, 50)
-my_compressor.scatterPlot2D('density', size=50)
-my_compressor.scatterPlot2D('radial', size=50)
-my_compressor.scatterPlot2D('tangential', size=50)
-my_compressor.scatterPlot2D('axial', size=50)
-my_compressor.scatterPlot2D('pressure', size=50)
+# #circumferential average and 2D plots
+# my_compressor.UnstructuredCircumferentialAverage(50, 50)
+# my_compressor.scatterPlot2D('density', size=50)
+# my_compressor.scatterPlot2D('radial', size=50)
+# my_compressor.scatterPlot2D('tangential', size=50)
+# my_compressor.scatterPlot2D('axial', size=50)
+# my_compressor.scatterPlot2D('pressure', size=50)
+
+import numpy as np
+OA = np.array(([0],
+               [0]))
+OB = np.array(([1],
+               [1]))
+OC = np.array(([0.8],
+               [1]))
+OD = np.array(([0],
+               [0.6]))
+AD = OA+(OD-OA)*np.linspace(0,1,100)
+BC = OB+(OC-OB)*np.linspace(0,1,100)
+plt.figure()
+plt.plot(AD[0,:], AD[1,:], label='AD')
+plt.plot(BC[0,:], BC[1,:], label='BC')
+plt.legend()
+
 
 
 

Sun/main_duct_resonance.py
@@ -24,12 +24,12 @@ gmma = 1.4                              #cp/cv ratio of air
 rho = p/(R*T)                           #density [kg/m3]
 a = np.sqrt(gmma*p/rho)                 #ideal speed of sound [m/s]
 
-#%% ANALYTICAL PART 
+#%% ANALYTICAL PART OF THE PROBLEM
 
-# #radial cordinate array span
-# r = np.linspace(r1,r2,250)
+#radial cordinate array span
+r = np.linspace(r1,r2,300)
 
-# #import Bessel functions
+#import Bessel functions
 # x = np.linspace(0,100,1000)
 # J1 = jv(1, x) #BF of the first kind of the first order
 # N1 = yv(1, x) #BF of the second kind of the first order
@@ -57,30 +57,47 @@ a = np.sqrt(gmma*p/rho)                 #ideal speed of sound [m/s]
 # ax[1,1].set_ylim([-1,1])
 # fig.suptitle("Bessel Functions")
 
-# #%% #analytical eigenvalues
-# lambda_mn = np.linspace(0,300,250) #we will do a loop for every possible value of lambda, but now let's evalute the determinant
+# #analytical eigenvalues
+# lambda_mn_span = np.linspace(0,300,300) #we will do a loop for every possible value of lambda
 # m = 1 
 # det = np.array(())
-# for i in range(0,len(lambda_mn)):
-#     Jm = jv(m, lambda_mn[i]*r)
-#     Nm = yv(m, lambda_mn[i]*r)
-#     # dJmdr = np.gradient(Jm, r)
-#     # dNmdr = np.gradient(Nm, r)
-#     dJmdr = jvp(m, lambda_mn[i]*r)
-#     dNmdr = yvp(m, lambda_mn[i]*r)
-#     det = np.append(det,dJmdr[0]*dNmdr[-1]-dNmdr[0]*dJmdr[-1])
+# det2 = np.array(())
+# det3 = np.array(())
+
+# for s in range(0,len(lambda_mn_span)):
+#     lambda_mn = lambda_mn_span[s]
+#     J1 = jvp(m, lambda_mn*r, n=0)
+#     N1 = yvp(m, lambda_mn*r, n=0)
+#     dJ1dr = jvp(m, lambda_mn*r, n=1)
+#     dN1dr = yvp(m, lambda_mn*r, n=1)
+#     det = np.append(det, dJ1dr[0]*dN1dr[-1]-dN1dr[0]*dJ1dr[-1])
+    
+#     J2 = jvp(m+1, lambda_mn*r, n=0)
+#     N2 = yvp(m+1, lambda_mn*r, n=0)
+#     dJ2dr = jvp(m+1, lambda_mn*r, n=1)
+#     dN2dr = yvp(m+1, lambda_mn*r, n=1)
+#     det2 = np.append(det2, dJ2dr[0]*dN2dr[-1]-dN2dr[0]*dJ2dr[-1])
+    
+#     J3 = jvp(m+2, lambda_mn*r, n=0)
+#     N3 = yvp(m+2, lambda_mn*r, n=0)
+#     dJ3dr = jvp(m+2, lambda_mn*r, n=1)
+#     dN3dr = yvp(m+2, lambda_mn*r, n=1)
+#     det3 = np.append(det3, dJ3dr[0]*dN3dr[-1]-dN3dr[0]*dJ3dr[-1])
 # zeros = np.zeros(len(det))
 
+# #plot the determinant value
 # fig, ax = plt.subplots(figsize=(10,7))
-# ax.plot(lambda_mn, det)
-# ax.plot(lambda_mn, zeros,'--k', lw=0.5)
+# ax.plot(lambda_mn_span, det, label=r'$m=1$')
+# ax.plot(lambda_mn_span, det2, label=r'$m=2$')
+# ax.plot(lambda_mn_span, det3, label=r'$m=3$')
+# ax.plot(lambda_mn_span, zeros,'--k', lw=0.5)
 # ax.set_title('determinant value')
 # ax.set_xlabel(r'$\lambda_{mn}$')
 # ax.set_ylim([-0.25,0.25])
 # ax.set_ylabel(r'$\det{A}$')
 # ax.legend()
 
-# #this is valid only for the first frequency, then you need to modify omega evaluation
+#this is valid only for the first frequency, then you need to modify omega evaluation
 # omega = a*np.sqrt(((1-M**2)*m*np.pi/L)**2 + (1-M**2)*lambda_mn**2) #alpha is the number of the eigenvalue
 # fig, ax = plt.subplots(figsize=(10,7))
 # ax.plot(omega, np.abs(det))
@@ -95,8 +112,8 @@ a = np.sqrt(gmma*p/rho)                 #ideal speed of sound [m/s]
 #%%COMPUTATIONAL PART
 
 #number of grid nodes in the computational domain
-Nz = 20
-Nr = 20
+Nz = 15
+Nr = 5
 
 #implement a constant uniform flow in the annulus duct
 density = np.random.rand(Nz, Nr)
@@ -120,102 +137,28 @@ for ii in range(0,Nz):
 duct = DataGrid(0, L, r1, r2, Nz, Nr, density, axialVel, radialVel, tangentialVel, pressure)
 
 
+#general workflow
+sunObj = SunModel(duct)
+sunObj.ShowPhysicalGrid(save_filename='physical_grid_%1.d_%1.d' %(Nz,Nr))
+sunObj.ComputeSpectralGrid()
+sunObj.ShowSpectralGrid(save_filename='spectral_grid_%1.d_%1.d' %(Nz,Nr))
+sunObj.ComputeJacobianSpectral(refinement=10)
+sunObj.AddAMatrixToNodes()
+sunObj.AddBMatrixToNodes()
+sunObj.AddCMatrixToNodes()
+sunObj.AddEMatrixToNodes()
+sunObj.AddRMatrixToNodes()
+sunObj.AddSMatrixToNodes()
+sunObj.AddHatMatricesToNodes()
+sunObj.ApplySpectralDifferentiation()
 
-# omega_range = np.linspace(10000, 35000, 150) #domain of interest, omega on
-# chi = np.zeros(len(omega_range))
-# Q_container =[]
-# for kk in range(0,len(omega_range)):
-#     print('SVD %.1d of %1.d' %(kk+1,len(omega_range)))
-#     sunObj = SunModel(duct)
-#     if kk==0:
-#         sunObj.ShowPhysicalGrid(save_filename='physical_grid_%1.d_%1.d' %(Nz,Nr))
-#     sunObj.ComputeSpectralGrid()
-#     if kk==0:
-#         sunObj.ShowSpectralGrid(save_filename='spectral_grid_%1.d_%1.d' %(Nz,Nr))
-#     sunObj.ComputeJacobianSpectral()
-#     if kk==0:
-#         sunObj.ShowJacobianPhysicalAxis(save_filename='spectral_jacobian_%1.d_%1.d' %(Nz,Nr))
-#     omega = omega_range[kk]
-#     sunObj.AddAMatrixToNodes(omega)
-#     sunObj.AddBMatrixToNodes()
-#     sunObj.AddCMatrixToNodes()
-#     sunObj.AddEMatrixToNodes()
-#     sunObj.AddRMatrixToNodes()
-#     sunObj.AddHatMatricesToNodes()
-#     sunObj.ApplySpectralDifferentiation()
-#     sunObj.AddRemainingMatrices()
-#     sunObj.ApplyBoundaryConditions() 
-#     # Q_container.append(sunObj.Q)
-#     u,s,v = np.linalg.svd(sunObj.Q)
-#     chi[kk] = np.min(s)/np.max(s)
-    
-
-#debug
-# for s in range(0,len(Q_container)):
-#     fig, ax = plt.subplots(1, 2, figsize=(16, 8))
-#     image1 = ax[0].imshow(Q_container[s].real)
-#     ax[0].set_title('Q.real, omega: %.1d' % (omega_range[s]))
-#     colorbar1 = fig.colorbar(image1, ax=ax[0])
-#     image2 = ax[1].imshow(Q_container[s].imag)
-#     ax[1].set_title('Q.imag, omega: %.1d' % (omega_range[s]))
-#     colorbar2 = fig.colorbar(image2, ax=ax[1])
-    
-# plt.figure(figsize=(10,6))
-# plt.plot(omega_range,chi)
-# plt.ylabel(r'$\chi$')
-# plt.xlabel(r'$\omega \ [rad/s]$')
-# plt.savefig('pictures/chi_map_%1.d_%1.d.pdf' %(Nz,Nr),bbox_inches='tight')
-
-#%% 2D OMEGA DOMAIN
-# omega_range_r = np.linspace(10000, 35000, 50) #domain of interest, omega on
-# omega_range_i = np.linspace(-8000, 8000, 50) #domain of interest, omega on
-
-omega_range_r = np.linspace(10000, 35000, 25) #domain of interest, omega on
-omega_range_i = np.linspace(-8000, 8000, 25) #domain of interest, omega on
-
-chi = np.zeros((len(omega_range_r),(len(omega_range_i))))
-for ii in range(0,len(omega_range_r)):
-    for jj in range(0,len(omega_range_i)):
-        print('SVD %.1d of %1.d' %(ii*len(omega_range_i)+1+jj,len(omega_range_r)*len(omega_range_i)))
-        sunObj = SunModel(duct)
-        if (ii==0 and jj==0):
-            sunObj.ShowPhysicalGrid(save_filename='physical_grid_%1.d_%1.d' %(Nz,Nr))
-        sunObj.ComputeSpectralGrid()
-        if (ii==0 and jj==0):
-            sunObj.ShowSpectralGrid(save_filename='spectral_grid_%1.d_%1.d' %(Nz,Nr))
-        sunObj.ComputeJacobianSpectral(refinement=10)
-        if (ii==0 and jj==0):
-            # sunObj.ShowJacobianPhysicalAxisFine(save_filename='spectral_jacobian_fine_%1.d_%1.d' %(Nz,Nr))
-            sunObj.ShowJacobianPhysicalAxis(save_filename='spectral_jacobian_%1.d_%1.d' %(Nz,Nr))
-        omega = omega_range_r[ii]+1j*omega_range_i[jj]
-        sunObj.AddAMatrixToNodes(omega)
-        sunObj.AddBMatrixToNodes()
-        sunObj.AddCMatrixToNodes()
-        sunObj.AddEMatrixToNodes()
-        sunObj.AddRMatrixToNodes()
-        sunObj.AddSMatrixToNodes()
-        sunObj.AddHatMatricesToNodes()
-        sunObj.ApplySpectralDifferentiation()
-        sunObj.AddRemainingMatrices()
-        sunObj.ApplyBoundaryConditions() 
-        u,s,v = np.linalg.svd(sunObj.Q)
-        chi[ii,jj] = np.min(s)/np.max(s)
-
-OM_I, OM_R = np.meshgrid(omega_range_i, omega_range_r)
-plt.figure(figsize=(10,6))
-plt.contourf(OM_R,OM_I,chi, cmap='viridis')
-plt.ylabel(r'$\omega_{i}$')
-plt.xlabel(r'$\omega_{r}$')
-plt.colorbar()
-plt.title(r'$\chi$')
-plt.savefig('pictures/chi_2D_map_%1.d_%1.d.pdf' %(Nz,Nr),bbox_inches='tight')
+sunObj.ComputeSVD(omega_domain=[7.5e3, 35e3, -8e3, 8e3], grid_omega=[200,10])
+sunObj.PlotInverseConditionNumber('chi_2D_map_%1.d_%1.d' %(Nz,Nr))
 
 
 
-
-
-
-
+plt.figure()
+plt.plot(sunObj.omegaR,sunObj.chi[:,1])
 
 
 

Sun/src/general_functions.py
@@ -80,39 +80,52 @@ def JacobianTransform(X,Y,Z,R):
 
 
 
-def ChebyshevDerivativeMatrixBayliss(x):
+def ChebyshevDerivativeMatrix(x):
     """
-    Define the first order derivative matrix, where x is the array of Gauss-Lobatto points. Given an x-array of cordinates
-    (that should be located on the gauss lobatto points), it returns the first order derivative operator. It is based on the 
-    Bayliss formulation, which artificially fix the diagonal elements.
+    Define the first order derivative Chebyshev matrix, where x is the array of Gauss-Lobatto points. Expression from 
+    Peyret book, page 50
     """
     N = len(x) #dimension of the square matrix
     D = np.zeros((N,N))
     for i in range(0,N):
         for j in range(0,N):
-            xi = np.cos(np.pi*i/N)
-            xj = np.cos(np.pi*j/N)
-            # compute off-diagonal before
+            xi = np.cos(np.pi*i/(N-1))
+            xj = np.cos(np.pi*j/(N-1))
+            
+            #select the right ci, cj
+            if i==0 or i==N-1:
+                ci = 2
+            else:
+                ci = 1
+            if j==0 or j==N-1:
+                cj = 2
+            else:
+                cj = 1
+            
+            #matrix coefficients
             if (i!=j):
-                if (i==0 or i==N):
-                    ci = 2
-                elif (i>0 or i <N):
-                    ci =1
-                else:
-                    raise ValueError('Error in the chebyshev derivative matrix formulation')
-                
-                if (j==0 or j==N):
-                    cj = 2
-                elif (j>0 or j<N):
-                    cj =1
-                else:
-                    raise ValueError('Error in the chebyshev derivative matrix formulation')
-                    
-                D[i,j] = (ci/cj)*(-1)**(i+j)/(xi-xj)
-                
+                D[i,j] = (ci/cj) * (-1)**(i+j) / (xi-xj)
+            elif (i==j and i>0 and i<N-1):
+                D[i,j] = -xi/2/(1-xi**2)
+            elif (i==0 and j==0):
+                D[i,j] = (2*(N**2)+1)/6
+            elif(i==N-1 and j==N-1):
+                D[i,j] = -(2*(N**2)+1)/6
+            else:
+                raise ValueError('Some mistake in the computation of the matrix')
+            
+    return D
+
+def ChebyshevDerivativeMatrixBayliss(x):
+    """
+    Define the first order derivative Chebyshev matrix, where x is the array of Gauss-Lobatto points. Expression from 
+    Peyret book, page 50. Bayliss formulation for the diagonal term, as suggested by Peyret to fix the extremes
+    """
+    N = len(x) #dimension of the square matrix
+    D = ChebyshevDerivativeMatrix(x) #basic
     for i in range(0,N):
-        tot_coeff = np.sum(D[i,:])
-        D[i,i] = - tot_coeff #artifially fix the diagonal element, so that the sum of the elements in a generic row gives zero
+        row_tot = np.sum(D[i,:]) - D[i,i] #sum of the terms out of the diagonal
+        D[i,i] = -row_tot #to make the sum of the elements in a row = 0 for every row
     return D
 
 
@@ -146,8 +159,8 @@ def GaussLobattoPoints(N):
     return x
         
       
-        
-        
+for i in range(0,5):
+    print(i)
         
         
         

Sun/src/sun_model.py
@@ -11,6 +11,7 @@ from src.grid import DataGrid
 from src.general_functions import JacobianTransform, ChebyshevDerivativeMatrixBayliss, Refinement, GaussLobattoPoints
 from src.styles import *
 import os
+import time
 
 
 class SunModel:
@@ -129,14 +130,6 @@ class SunModel:
                 #add the inverse gradients information to every node
                 self.data.dataSet[ii,jj].AddInverseJacobianGradients(self.dzdx[ii,jj], self.dzdy[ii,jj], self.drdx[ii,jj], self.drdy[ii,jj])
     
-    
-        
-        
-        
-        
-        
-        
-        
         
     
     def ShowJacobianSpectralAxis(self, formatFig=(10,6)):
@@ -264,49 +257,56 @@ class SunModel:
         if save_filename!=None:
             plt.savefig(folder_name + save_filename+'_4.pdf',bbox_inches='tight')
     
-    # def ComputeSVD(self, omega_domain=[-1,1,-1,1], grid_omega=[10,10]):
-    #     omR_min = omega_domain[0]
-    #     omR_max = omega_domain[1]
-    #     omI_min = omega_domain[2]
-    #     omI_max = omega_domain[3]
-    #     nR = grid_omega[0]
-    #     nI = grid_omega[1]
-    #     omR = np.linspace(omR_min, omR_max, nR)
-    #     omI = np.linspace(omI_min, omI_max, nI)
-    #     self.omegaR, self.omegaI = np.meshgrid(omR, omI)
-    #     self.chi = np.zeros((nR,nI))
-    #     for ii in range(0,nR):
-    #         for jj in range(0,nI):
-    #             omega = omR[ii]+1j*omI[jj]
-                
-    #             #to be implemented correctly later
-    #             Q = 1j*omega*self.A+self.B+self.C+self.E+self.R+self.S
+    def ComputeSVD(self, omega_domain, grid_omega=[10,10]):
+        omR_min = omega_domain[0]
+        omR_max = omega_domain[1]
+        omI_min = omega_domain[2]
+        omI_max = omega_domain[3]
+        nR = grid_omega[0]
+        nI = grid_omega[1]
+        omR = np.linspace(omR_min, omR_max, nR)
+        omI = np.linspace(omI_min, omI_max, nI)
+        self.omegaI, self.omegaR = np.meshgrid(omI, omR)
+        self.chi = np.zeros((nR,nI))
+        start_time = time.time()
+        for ii in range(0,nR):
+            for jj in range(0,nI):
+                current_time = time.time() - start_time
+                if (ii==0 and jj==1): 
+                    delta_time_svd = current_time
+                    total_time = delta_time_svd*nR*nI
+                if (ii>=0 and jj>=1):
+                    remaining_minutes = (total_time-current_time)/60
+                    total_minutes = total_time/60
+                    print('SVD %.1d of %1.d (%.1d of %1.d minutes remaining)' %(ii*len(omI)+1+jj,len(omR)*len(omI),remaining_minutes, total_minutes)) #keep track of the progress
 
-    #             U,S,V = np.linalg.svd(Q)
-    #             self.chi[ii,jj] = np.min(S)/np.max(S)
-    
-    
-    # def PlotInverseConditionNumber(self, save_filename=None, formatFig=(10,6)):
-    #     x = np.linspace(np.min(self.omegaR), np.max(self.omegaR))
-    #     critical_line = np.zeros(len(x))
-    #     plt.figure(figsize=formatFig)
-    #     plt.contourf(self.omegaR, self.omegaI, self.chi)
-    #     plt.plot(x, critical_line, '--r')
-    #     plt.xlabel(r'$\omega_{R}$')
-    #     plt.ylabel(r'$\omega_{I}$')
-    #     plt.title(r'$\chi$ locus')
-    #     cb = plt.colorbar()
-    #     cb.set_label(r'$\chi$')
-    #     if save_filename!=None:
-    #         plt.savefig(folder_name + save_filename +'.pdf' ,bbox_inches='tight')
+                omega = omR[ii]+1j*omI[jj]
+                self.AddRemainingMatrices(omega) #add the non-constant parts of the matrices
+                self.ApplyBoundaryConditions()  #apply boundary condtions
+                u,s,v = np.linalg.svd(self.Qtot)
+                self.chi[ii,jj] = np.min(s)/np.max(s)
+
+    def PlotInverseConditionNumber(self, save_filename=None, formatFig=(10,6)):
+        x = np.linspace(np.min(self.omegaR), np.max(self.omegaR))
+        critical_line = np.zeros(len(x))
+        plt.figure(figsize=formatFig)
+        plt.contourf(self.omegaR, self.omegaI, self.chi/np.max(self.chi))
+        plt.plot(x, critical_line, '--r')
+        plt.xlabel(r'$\omega_{R}$')
+        plt.ylabel(r'$\omega_{I}$')
+        plt.title(r'$\chi / \chi_{max}$')
+        cb = plt.colorbar()
+        # cb.set_label()
+        if save_filename!=None:
+            plt.savefig(folder_name + save_filename +'.pdf' ,bbox_inches='tight')
                 
-    def AddAMatrixToNodes(self, omega):
+    def AddAMatrixToNodes(self):
         """
-        compute and store at the node level the A matrix, already multiplied by j*omega. Ready to be used in the final system of eqs.
+        compute and store at the node level the A matrix, not already multiplied by j*omega.
         """
         for ii in range(0,self.data.nAxialNodes):
             for jj in range(0,self.data.nRadialNodes):
-                A = np.eye(5, dtype=complex)*1j*omega
+                A = np.eye(5, dtype=complex)
                 self.data.dataSet[ii,jj].AddAMatrix(A)
                 
     def AddBMatrixToNodes(self):
@@ -525,6 +525,12 @@ class SunModel:
         add elements to the stability matrix specifying the first top-left element location
         """
         self.Q[row:row+5, column:column+5] += block
+        
+    def AddToQ_var(self, block, row, column):
+        """
+        add elements to the stability matrix specifying the first top-left element location
+        """
+        self.Q_var[row:row+5, column:column+5] += block
             
         
     def ApplyBoundaryConditions(self):
@@ -547,41 +553,42 @@ class SunModel:
                 elif (marker != ''):
                     raise Exception('Boundary condition unknown. Check the grid markers!')
                     
-    def AddRemainingMatrices(self):
+    def AddRemainingMatrices(self, omega):
         """
-        it adds the remaning block matrices to the full Q, on the diagonal blocks.
+        it adds the remaning diagonal block matrices to the full Qtot = Q + Q_var. Q is constant for every model, while Q_var depends on omega
         """
+        self.Q_var = np.zeros((self.Q.shape), dtype=complex) #variable part of the stability matrix
         for ii in range(0,self.dataSpectral.nAxialNodes):
             for jj in range(0,self.dataSpectral.nRadialNodes):
                 #add all the remaining terms on the diagonal
-                diag_block_ij = self.data.dataSet[ii,jj].A + self.data.dataSet[ii,jj].C + self.data.dataSet[ii,jj].R + self.data.dataSet[ii,jj].S
-                
+                diag_block_ij = self.data.dataSet[ii,jj].A*1j*omega + self.data.dataSet[ii,jj].C + self.data.dataSet[ii,jj].R + self.data.dataSet[ii,jj].S
                 node_counter = self.data.dataSet[ii,jj].nodeCounter
                 row = node_counter*5
                 column = node_counter*5
-                self.AddToQ(diag_block_ij, row, column)
+                self.AddToQ_var(diag_block_ij, row, column)
+        self.Qtot = self.Q + self.Q_var #the global stability
     
     def ApplyInletCondition(self, row):
         """
         for the considered grid node, it substitutes the 5 equations with a zero perturbation condition
         """
-        self.Q[row:row+5,:] = np.zeros(self.Q[row:row+5,:].shape, dtype=complex) #make it zero first
-        self.Q[row:row+5,row:row+5] = np.eye(5, dtype=complex) #zero perturbation condition for every flow variable
+        # self.Qtot[row:row+5,:] = np.zeros(self.Qtot[row:row+5,:].shape, dtype=complex) #make it zero first
+        # self.Qtot[row:row+5,row:row+5] = np.eye(5, dtype=complex) #zero perturbation condition for every flow variable
         
     
     def ApplyOutletCondition(self, row):
         """
         for the considered grid node, it substitutes the pressure equation with a zero pressure condition
         """
-        self.Q[row+4,:] = np.zeros(self.Q[row+4,:].shape, dtype=complex) #make first the pressure equation coefficients zero
-        self.Q[row+4,row+4] = 1 #apply zero pressure condition
+        # self.Qtot[row+4,:] = np.zeros(self.Qtot[row+4,:].shape, dtype=complex) #make first the pressure equation coefficients zero
+        # self.Qtot[row+4,row+4] = 1 #apply zero pressure condition
         
     def ApplyWallCondition(self, row, wall_normal = [1, 0, 0]):
         """
         for the considered grid node, it substitutes one of the velocity equation with a non-penetration condition
         """
-        self.Q[row+1,:] = np.zeros(self.Q[row+1,:].shape, dtype=complex) #first make zero the radial velocity equation
-        self.Q[row+1,row+1:row+4] = wall_normal #impose non-penetration condition (u*nr + v*nt + w*nz)
+        self.Qtot[row+1,:] = np.zeros(self.Qtot[row+1,:].shape, dtype=complex) #first make zero the radial velocity equation
+        self.Qtot[row+1,row+1:row+4] = wall_normal #impose non-penetration condition (u*nr + v*nt + w*nz)
         
 
         